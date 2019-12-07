5cd1b9a517262aa2bcfe017a1f5339809600ac3b
Mihai Smarandache <mihai.sanfran@gmail.com>
2019-12-07 16:59:48 -0700

    Create request handler for creating a user

--- src/index.js
+++ src/index.js
@@ -6,6 +6,15 @@ const app = express()
 // Get default port; For deployment on Heroku; or localhost
 const port = process.env.PORT || 3000
 
+// Automatically parse data as a JSON object in all request handlers
+app.use(express.json())
+
+// Create method for creating a new user
+app.post('/users', (req, res) => {
+    console.log(req.body)
+    res.send('testing')
+})
+
 app.listen(port, () => {
     console.log('Server is up on port ', + port)
 })
