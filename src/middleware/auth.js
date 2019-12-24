c633f1c0effd53defbd8f9c3984db90cc2f6cc59
Mihai Smarandache <mihai.sanfran@gmail.com>
2019-12-23 17:48:58 -0700

    Create route handler for fetching user profile with authentication

---
+++ src/middleware/auth.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
+
+// Middleware to authenticate user
+const auth = async (req, res, next) => {
+   try {
+       // Access token from header of request
+        const token = req.header('Authorization').replace('Bearer ', '')
+        const decoded = jwt.verify(token, 'thisisasecret') // Use same secret used to generate it
+        
+        // User ID was used to generate the token and can be retrieved during decoding
+        // Also check if this token is still part of the users tokens array
+        const user = await User.findOne({ _id: decoded._id, 'tokens.token': token })
+
+        if(!user){
+            throw new Error()
+        }
+
+        req.user = user
+
+        // Run route handler
+        next()
+   } catch (e) {
+        // If user is not authenticated
+        res.status(401).send({ error: 'Please authenticate.'})
+   }
+}
+
+module.exports = auth
