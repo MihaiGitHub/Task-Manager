e830f2b2fd36c103ebe335677cf78dd87461decf
Mihai Smarandache <mihai.sanfran@gmail.com>
2019-12-28 21:31:15 -0700

    Send email on user creation/signup

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,2 +1,19 @@
-const sendgridAPIKey = ''
+const sgMail = require('@sendgrid/mail')
 
+const sendgridAPIKey = 'xxxx'
+
+sgMail.setApiKey(sendgridAPIKey)
+
+const sendWelcomeEmail = (email, name) => {
+    sgMail.send({
+        to: 'mihai.sanfran@gmail.com',
+        from: 'mihai.sanfran@gmail.com',
+        subject: 'Thanks for joining!',
+        text: `Welcome to the app, ${name}!`,
+       // html: ''
+    })
+}
+
+module.exports = {
+    sendWelcomeEmail
+}
