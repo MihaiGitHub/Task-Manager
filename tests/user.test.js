3a987fc4b11f51d9a655f25fee5d16f7dae31ceb
Mihai Smarandache <mihai.sanfran@gmail.com>
2019-12-31 22:19:02 -0700

    Delete all users before running each test case

--- tests/user.test.js
+++ tests/user.test.js
@@ -3,6 +3,20 @@ const request = require('supertest')
 /* Load app for testing purpose */
 const app = require('../src/app')
 
+/* Load in User model for testing */
+const User = require('../src/models/user')
+
+/* Function to run before each test case in this test suite */
+beforeEach(async () => {
+    /* Delete all users before running each test case */
+    await User.deleteMany()
+})
+
+/* Function to run after each test case in this test suite */
+afterEach(() => {
+    console.log('after each')
+})
+
 test('Should signup a new user', async () => {
 
     // Send data object to user signup endpoint
