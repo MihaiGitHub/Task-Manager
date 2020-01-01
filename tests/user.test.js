fd35c07c541ea6d21f03cf46a2881a5d128c58bf
Mihai Smarandache <mihai.sanfran@gmail.com>
2019-12-31 21:02:49 -0700

    Write test case for user sign up endpoint

--- tests/user.test.js
+++ tests/user.test.js
@@ -2,3 +2,14 @@ const request = require('supertest')
 
 /* Load app for testing purpose */
 const app = require('../src/app')
+
+test('Should signup a new user', async () => {
+
+    // Send data object to user signup endpoint
+    await request(app).post('/users').send({
+        name: 'Fedor',
+        email: 'fedor@yahoo.com',
+        password: 'Hdhss87$#'
+    }).expect(201)
+
+})
